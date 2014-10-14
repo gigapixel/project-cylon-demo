10a45a5560ee77634edd5896fb6940dda58b9ddb
Peerapat Sungkasem <gigapixel7@gmail.com>
2014-10-14 21:09:11 +0700

    update project

--- features/environment.py
+++ features/environment.py
@@ -6,18 +6,32 @@ from project_cylon.CommonSteps import *
 from project_cylon.World import World as world
 
 def before_all(context):
-    browser = "firefox"
-
-    site_config = "default"
-    pageobject_files = "./pageobjects/*.yaml"
-
     Logger.tracebacklimit(0)
+    if context.config.debug == True:
+        Logger.tracebacklimit(1000)
 
-    if PageFactory.check_yaml_syntax(pageobject_files) == True:
-        world.pages = PageFactory.create_pages(pageobject_files, site_config)
-        world.open_browser(browser)
+    if context.config.screenshot == True:
+        Logger.enable_screenshots()
+
+    site = "default"
+    if hasattr(context.config, "site") and context.config.site is not None:
+        site = context.config.site
+
+    domain = PageFactory.get_domain("./config.yaml", site)
+
+    pageobjects = "./pageobjects/*.yaml"
+    if PageFactory.check_yaml_syntax(pageobjects) == True:
+        world.pages = PageFactory.create_pages(pageobjects, domain)
     else:
         Logger.failed("Stop running.")
 
-def after_all(context):
+def before_feature(context, feature):
+    browser = "firefox"
+    if hasattr(context.config, "browser") and context.config.browser is not None:
+        browser = context.config.browser
+    world.open_browser(browser)
+
+    Logger.driver = world.driver
+
+def after_feature(context, feature):
     world.close_browser()
